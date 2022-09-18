677e2b5bf722727a557c8b1510587640abe43a63
Ji Hun Wang <wang1227@stanford.edu>
2022-09-18 13:43:46 +0900

    Initial Commit

---
+++ autospotify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  autospotify
+//
+//  Created by Ji Hun Wang on 2022/09/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ autospotify/autospotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  autospotifyApp.swift
+//  autospotify
+//
+//  Created by Ji Hun Wang on 2022/09/18.
+//
+
+import SwiftUI
+
+@main
+struct autospotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
